d8ef01a1323f8f1f15fa323f522faa1ebc2399b0
Alex Mendes <22567760+alexolinux@users.noreply.github.com>
2024-12-13 16:58:40 +0000

    [FEAT] refactor the Project
    This commit does:
    - Adjustment needed for appending to plugins
    - Remove operating system requirements
    Fix the complete execution

--- install.sh
+++ install.sh
@@ -1,193 +1,85 @@
 #!/bin/bash
 
-GROUP_NAME="devops"
-GROUP_SUDO="/etc/sudoers.d/devops"
 ZSHRC_FILE="${HOME}/.zshrc"
+OH_MYZSH="${HOME}/.oh-my-zsh"
+ZSH_PLUGINS_DIR="${OH_MYZSH}/plugins"
 ZSH_PLUGINS=(
     zsh-syntax-highlighting
     zsh-autosuggestions
 )
 
-# Create a temporary file to hold the modified content
-TEMP_FILE=$(mktemp)
-
-# Detect OS type
-OS_TYPE=$(uname -s)
-
-# Check package manager
-get_package_manager() {
-    if [[ "$OS_TYPE" == "Linux" ]]; then
-        if command -v apt &> /dev/null; then
-            echo "apt"
-        elif command -v yum &> /dev/null; then
-            echo "yum"
-        elif command -v dnf &> /dev/null; then
-            echo "dnf"
-        else
-            echo "Unsupported package manager"
-            exit 1
-        fi
-    elif [[ "$OS_TYPE" == "Darwin" ]]; then
-        if command -v brew &> /dev/null; then
-            echo "brew"
-        else
-            echo "brew_not_installed"
-        fi
-    else
-        echo "Unsupported operating system"
+check_comm() {
+    if [ "$?" -ne 0 ]; then
+        echo "Command failed. Script interrupted."
         exit 1
     fi
 }
 
-# Install Homebrew if not present (macOS only)
-install_homebrew() {
-    echo "Checking Homebrew installation..."
-    if ! command -v brew &> /dev/null; then
-        echo "Homebrew not found. Installing..."
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    else
-        echo "Homebrew is already installed."
-    fi
-}
+clear && echo "#-- ----------|| ZSH SHELL Configuration ||---------- --#"
 
-# Function to install packages based on package manager
-install_packages() {
-    local package_manager=$1
-
-    case $package_manager in
-        "apt")
-            sudo apt update
-            sudo apt upgrade -y
-            sudo apt install -y git gcc g++ autoconf make automake wget curl passwd vim-common openssl jq zsh
-            ;;
-        "yum")
-            sudo yum update -y
-            sudo yum install -y git gcc gcc-c++ autoconf make automake wget curl util-linux vim-common openssl jq zsh
-            ;;
-        "dnf")
-            sudo dnf update -y
-            sudo dnf install -y git gcc gcc-c++ autoconf make automake wget curl util-linux-user vim-common openssl jq zsh
-            ;;
-        "brew")
-            brew update
-            brew install git gcc autoconf automake wget curl vim openssl jq zsh
-            ;;
-        *)
-            echo "Unsupported package manager"
-            exit 1
-            ;;
-    esac
-}
-
-# Determine package manager
-package_manager=$(get_package_manager)
-
-if [[ "$package_manager" == "brew_not_installed" ]]; then
-    install_homebrew
-    package_manager="brew"
-fi
-
-echo "Update system"
-# Install required packages based on package manager
-install_packages $package_manager
-
-if [[ "$OS_TYPE" == "Linux" ]]; then
-    # Check if the group exists
-    if grep -q "^$GROUP_NAME:" /etc/group; then
-        echo "Group $GROUP_NAME already exists."
-    else
-        # Group does not exist, create it
-        echo "Group $GROUP_NAME does not exist. Creating..."
-        sudo groupadd -g 2024 $GROUP_NAME
-        echo "Group $GROUP_NAME created."
-    fi
-
-    # Check if the user is already a member of the group
-    if ! groups $USER | grep -q "\b$GROUP_NAME\b"; then
-        # User is not a member of the group, so add them
-        sudo usermod -aG $GROUP_NAME $USER
-        echo "$USER has been added to the group $GROUP_NAME."
-    else
-        echo "$USER is already a member of the group $GROUP_NAME."
-    fi
-
-    echo "Checking sudoers file for devops group"
-    if [ -e "$GROUP_SUDO" ]; then
-        echo "The file $GROUP_SUDO exists."
-    else
-        echo "%devops ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/devops
-        echo "Sudo has been applied for the $GROUP_NAME group."
-    fi
-fi
-
-echo "ZSH Installation"
-# Install ZSH and set it as the default shell if not installed
-if ! command -v zsh &> /dev/null; then
-    echo "ZSH not found. Installing..."
-    install_packages $package_manager
-fi
-
-if command -v chsh &> /dev/null; then
+if command -v chsh &> /dev/null; then    
     echo "Changing user SHELL to ZSH using chsh..."
     zsh_path=$(command -v zsh)
-    
-    if ! grep -q "$zsh_path" /etc/shells; then
-        echo "$zsh_path" | sudo tee -a /etc/shells
-    fi
-
-    chsh -s "$zsh_path"
+    sudo chsh -s "$zsh_path" "$USER"
 else
-    echo "chsh command not available. Please manually change your shell to ZSH."
+    echo "Failed to change User SHELL. Check if ZSH is available."
+    exit 1
 fi
 
 echo "Checking OHMyZSH Installation..."
-if [ ! -d "$HOME/.oh-my-zsh" ]; then
+if [ ! -d "${OH_MYZSH}" ]; then
     echo "Installing OHMyZSH..."
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    check_comm
 fi
 
-echo "Checking PowerLevel10K Installation..."
-if [ ! -d "$HOME/.oh-my-zsh/themes/powerlevel10k" ]; then
-    echo "Installing Powerlevel10k..."
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$HOME/.oh-my-zsh/themes/powerlevel10k"
+if [ ! -d "${ZSH_PLUGINS_DIR}" ]; then
+    mkdir -p "${ZSH_PLUGINS_DIR}"
 fi
-
-echo "Changing ZSH Theme..."
-sed -i '' 's/ZSH_THEME=.*/ZSH_THEME="powerlevel10k\/powerlevel10k"/' "$ZSHRC_FILE"
-
-plugins_dir="$HOME/.oh-my-zsh/plugins"
-mkdir -p "$plugins_dir"
 
 echo "Checking/Installing ZSH plugins..."
+
+if [ ! -d "${ZSH_PLUGINS_DIR}/zsh-autosuggestions" ]; then
+    echo "Installing zsh-autosuggestions..."
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git "${ZSH_PLUGINS_DIR}/zsh-autosuggestions"
+    check_comm
+fi
+
+if [ ! -d "${ZSH_PLUGINS_DIR}/zsh-syntax-highlighting" ]; then
+    echo "Installing zsh-syntax-highlighting..."
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_PLUGINS_DIR}/zsh-syntax-highlighting"
+    check_comm
+fi
+
+echo "ZSH Plugins downloaded successfully! \o/\o/"
+
+echo "Adding New plugins to the ZSH Environment..."
+
+ZSH_PLUGINS_STRING=""
 for plugin in "${ZSH_PLUGINS[@]}"; do
-    if [ ! -d "$plugins_dir/$plugin" ]; then
-        echo "Installing $plugin..."
-        git clone https://github.com/zsh-users/$plugin.git "$plugins_dir/$plugin"
-    fi
+    ZSH_PLUGINS_STRING+="  $plugin\n"
 done
 
-echo "Backing up ZSH User file configuration..."
-cp -p "${ZSHRC_FILE}" "${ZSHRC_FILE}.save" 
+sed -i.bak "/^plugins=(/{
+    s/^plugins=(/&\n$ZSH_PLUGINS_STRING/
+    s/\n\n/\n/g  # Remove any extra new lines
+}" "${ZSHRC_FILE}"
 
-echo "Appending new ZSH plugins..."
+check_comm
 
-inside_plugins_array=false
+echo "New plugins added to ${ZSHRC_FILE}. A backup of the original file is saved as ${ZSHRC_FILE}.bak."
 
-while IFS= read -r line; do
-    if [[ "$line" =~ ^plugins=\( ]]; then
-        inside_plugins_array=true
-        echo "$line" >> "$TEMP_FILE"
-    elif [[ "$line" =~ ^\) && $inside_plugins_array == true ]]; then
-        for plugin in "${ZSH_PLUGINS[@]}"; do
-            echo "    $plugin" >> "$TEMP_FILE"
-        done
-        echo "$line" >> "$TEMP_FILE"
-        inside_plugins_array=false
-    else
-        echo "$line" >> "$TEMP_FILE"
-    fi
-done < "$ZSHRC_FILE"
+echo "Migrating to PowerLevel10K..."
 
-mv "$TEMP_FILE" "$ZSHRC_FILE"
+if [ ! -d "${OH_MYZSH}/themes/powerlevel10k" ]; then
+    echo "Installing Powerlevel10k..."
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${OH_MYZSH}/themes/powerlevel10k"
+fi
 
-echo "New plugins added successfully."
+check_comm
+
+echo "Changing ZSH Theme..."
+sed -i 's/ZSH_THEME=.*/ZSH_THEME="powerlevel10k\/powerlevel10k"/' "$HOME/.zshrc"
+
+echo "Script completed successfully! \o/\o/ Reload you SHELL Environment to make effect."
+echo "Apply the changes executing: zsh"
